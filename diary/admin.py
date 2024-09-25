3a709acc43a45b79b43eafe9886bf943233af060
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-25 14:05:14 +0300

    добавление class DiaryAdmin

--- diary/admin.py
+++ diary/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from diary.models import Diary
+
+
+@admin.register(Diary)
+class DiaryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'context', 'date_event', 'created_at', 'is_active', 'owner')
