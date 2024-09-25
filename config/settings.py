66e4cd92a3195430b567b613cfc076eeae99bfa4
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-25 14:39:57 +0300

    Добавление MEDIA_URL, MEDIA_ROOT

--- config/settings.py
+++ config/settings.py
@@ -94,3 +94,6 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
