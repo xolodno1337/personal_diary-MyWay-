d0436b52d0e4110c9f322feab0a0485f0157610c
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-26 15:42:16 +0300

    добавлен crud diary

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from diary.models import Diary
 
 
-def home(request):
-    return render(request, 'home.html')
+def base(request):
+    return render(request, 'base.html')
+
+
+class DiaryListView(ListView):
+    model = Diary
+    template_name = 'diary/diary_list.html'
+
+
+class DiaryDetailView(DetailView):
+    model = Diary
+    template_name = 'diary/diary_detail.html'
+
+
+class DiaryCreateView(CreateView):
+    model = Diary
+    fields = ('title', 'image', 'context', 'date_event', 'is_active')
+    success_url = reverse_lazy('diary:diary_list')
+
+
+class DiaryUpdateView(UpdateView):
+    model = Diary
+    fields = ('title', 'image', 'context', 'date_event', 'is_active')
+    success_url = reverse_lazy('diary:diary_list')
+
+
+class DiaryDeleteView(DeleteView):
+    model = Diary
+    success_url = reverse_lazy('diary:diary_list')
