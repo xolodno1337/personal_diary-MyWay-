e0956eb8b63a9235eb2447687079647e2cdcc872
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-27 11:41:08 +0300

    Добавление class StyleFormMixin, class UserRegisterForm

---
+++ users/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm
+from users.models import User
+from django import forms
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, forms.BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2',)
