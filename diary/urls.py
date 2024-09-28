d29652e1faf9ab6cd4b3e1b8d998015cdcae2dff
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-28 13:19:27 +0300

    Добавление diary_personal

--- diary/urls.py
+++ diary/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from diary.apps import DiaryConfig
-from diary.views import base, DiaryListView, DiaryDetailView, DiaryCreateView, DiaryUpdateView, DiaryDeleteView
+from diary.views import base, DiaryListView, DiaryDetailView, DiaryCreateView, DiaryUpdateView, DiaryDeleteView, \
+    DiaryPersonalListView
 
 app_name = DiaryConfig.name
 
 urlpatterns = [
     path('', base, name='base'),
     path('diary/', DiaryListView.as_view(), name='diary_list'),
+    path('diary_personal/', DiaryPersonalListView.as_view(), name='diary_personal_list'),
     path('diary/create', DiaryCreateView.as_view(), name='diary_create'),
     path('diary/<int:pk>/', DiaryDetailView.as_view(), name='diary_detail'),
     path('diary/update/<int:pk>/', DiaryUpdateView.as_view(), name='diary_update'),
