a099ca471bdea8268f0b415348aebb5b41872de9
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-29 11:29:16 +0300

    добавление def reset_password

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 import secrets
+import string
+import random
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from config.settings import EMAIL_HOST_USER
 from django.core.mail import send_mail
 from django.urls import reverse_lazy, reverse
@@ -60,3 +62,28 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
 class UserDeleteView(LoginRequiredMixin, DeleteView):
     model = User
     success_url = reverse_lazy('diary:base')
+
+
+def reset_password(request):
+    context = {
+        'success_message': 'Пароль успешно сброшен. Новый пароль был отправлен на адрес Вашей электронной почты.',
+    }
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        user = get_object_or_404(User, email=email)
+        characters = string.ascii_letters + string.digits
+        characters_list = list(characters)
+        random.shuffle(characters_list)
+        password = ''.join(characters_list[:10])
+        user.set_password(password)
+        user.save()
+
+        send_mail(
+            subject='Восстановление пароля',
+            message=f'Ваш пароль для доступа на наш сайт изменен:\n {password}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return render(request, 'base.html', context)
+    return render(request, 'users/reset_password.html')
